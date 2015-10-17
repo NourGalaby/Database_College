3a5df90727121f7a4cb9601bfcf3fd6f91c5d23c
Nour Galaby <NourGalaby@users.noreply.github.com>
2015-10-17 06:29:54 +0200

    Update register.php

--- labs/register.php
+++ labs/register.php
@@ -64,7 +64,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
-                            <h3><strong>Lab1</strong> TITLE</h3>
+                         
                             <div class="description">
                             	<p>
 	                            	
