12940264c5c5e82bab67ab39a55bbd8de8d2beaa
Nour Galaby <NourGalaby@users.noreply.github.com>
2015-10-19 22:20:46 +0200

    Update connect.php
    added user and pass

--- labs/connect.php
+++ labs/connect.php
@@ -1,6 +1,6 @@
 
 <?php 
-$mysqli = new mysqli("us-cdbr-azure-southcentral-e.cloudapp.net", "b42b6620a25831", "f12df878", "ssp");
+$mysqli = new mysqli("us-cdbr-azure-southcentral-e.cloudapp.net", "b42b6620a25831", "fc38142e", "ssp");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
